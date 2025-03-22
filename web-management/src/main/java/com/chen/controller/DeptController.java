651204866af5c6b6022c49481783826aaa5dc06a
chen-1368 <ccpp159357258@gmail.com>
2025-03-22 18:56:15 +0800

    使用logback

--- web-management/src/main/java/com/chen/controller/DeptController.java
+++ web-management/src/main/java/com/chen/controller/DeptController.java
@@ -3,50 +3,49 @@ package com.chen.controller;
 import com.chen.pojo.Dept;
 import com.chen.pojo.Result;
 import com.chen.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
-
     @Autowired
     private DeptService deptService;
 
     @GetMapping
     public Result list(){
-        System.out.println("查询所有部门");
+        log.info("查询所有部门");
         List<Dept> deptList=deptService.findAll();
         return Result.success(deptList);
     }
 
     @DeleteMapping
     public Result delete(Integer id){
+        log.info("根据id删除数据，id为：{}",id);
         deptService.deleteById(id);
         return Result.success();
     }
 
     @PostMapping
     public Result add(@RequestBody Dept dept){
+        log.info("添加一个部门：{}",dept);
         deptService.add(dept);
         return Result.success();
     }
 
-    /**
-     * 根据id查询部门
-     */
     @GetMapping("/{id}")
     public Result getInfoById(@PathVariable Integer id){
-        System.out.println("需要查询的部门id是："+id);
+        log.info("根据id查询部门,id是: {}",id);
         Dept dept=deptService.getInfoById(id);
         return Result.success(dept);
     }
 
     @PutMapping
     public Result update(@RequestBody Dept dept){
-        System.out.println("修改的部门："+dept);
+        log.info("修改的部门：{}",dept);
         deptService.update(dept);
         return Result.success();
     }
